7be004740c73dd652e0d081ff633a5bede5d2815
Preeti <84238610+preeti003@users.noreply.github.com>
2021-10-15 01:47:31 +0530

    Create structures.cpp

---
+++ structures.cpp
@@ -0,0 +1,34 @@
+/*Input Format
+
+Input will consist of four lines.
+The first line will contain an integer, representing age.
+The second line will contain a string, consisting of lower-case Latin characters ('a'-'z'), representing the first_name of a student.
+The third line will contain another string, consisting of lower-case Latin characters ('a'-'z'), representing the last_name of a student.
+The fourth line will contain an integer, representing the standard of student.
+
+Note: The number of characters in first_name and last_name will not exceed 50.
+
+Output Format
+
+Output will be of a single line, consisting of age, first_name, last_name and standard, each separated by one white space.*/
+
+#include <iostream>
+
+
+using namespace std;
+
+struct Student{
+    int age;
+    string first_name;
+    string last_name;
+    int standard;
+};
+
+int main() {
+    Student st;
+    
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
+    return 0;
+}
