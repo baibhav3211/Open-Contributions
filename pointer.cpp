2b23612153f2270e5deb0276e69a1537f903978c
Preeti <84238610+preeti003@users.noreply.github.com>
2021-10-15 01:49:50 +0530

    Create pointer.cpp

---
+++ pointer.cpp
@@ -0,0 +1,31 @@
+/*Function Description
+
+update function has the following parameters:
+
+int *a: an integer
+int *b: an integer
+Returns
+
+The function is declared with a void return type, so there is no value to return. Modify the values in memory so that  contains their sum and  contains their absoluted difference.
+
+
+Input Format
+
+Input will contain two integers,  and , separated by a newline.*/
+
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
