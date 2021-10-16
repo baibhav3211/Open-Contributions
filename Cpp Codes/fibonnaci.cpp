91b33b76acde03f5bf208c095d40b42ff7894e97
electricsohan <92517195+electricsohan@users.noreply.github.com>
2021-10-16 16:42:11 +0530

    Create fibonnaci.cpp
    Fibonnaci series element finder with dynamic programming.

---
+++ Cpp Codes/fibonnaci.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fib(int n)
+{
+    int f[n + 2];
+    int i;
+    
+    f[0] = 0;
+    f[1] = 1;
+ 
+    for(i = 2; i <= n; i++)
+    {
+       f[i] = f[i - 1] + f[i - 2];
+    }
+    return f[n];
+}
+int main()
+{
+    int num,ans;
+    cout<<"Enter the position for which the element is to be found\n";
+    cin>>num;
+    ans=fib(num);
+    cout<<ans;
+    return 0;
+}
