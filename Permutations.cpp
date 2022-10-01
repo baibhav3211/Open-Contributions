ad1fe6694893b2a402ce0e162a761e9f330aa7f6
Vibespen <73059443+Vibespen@users.noreply.github.com>
2022-10-01 09:49:45 +0530

    Add files via upload

---
+++ Permutations.cpp
@@ -0,0 +1,42 @@
+// Given a string s, find all permutations of the string.
+
+// Input: abc
+// Output:
+// abc, acb, bac, bca, cab, cba
+
+// C++ program to print all
+// permutations with duplicates allowed
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void permute(string a, int l, int r)
+{
+	// Base case
+	if (l == r)
+		cout<<a<<endl;
+	else
+	{
+		// Permutations made
+		for (int i = l; i <= r; i++)
+		{
+
+			// Swapping done
+			swap(a[l], a[i]);
+
+			// Recursion called
+			permute(a, l+1, r);
+
+			//backtrack
+			swap(a[l], a[i]);
+		}
+	}
+}
+int main()
+{
+	string str = "ABC";
+	int n = str.size();
+	permute(str, 0, n-1);
+	return 0;
+}
+
