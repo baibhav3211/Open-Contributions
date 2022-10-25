9f62597ac08a7eb812b30131c424cb442cb1cf71
sparsh kumar <71932955+1234spa@users.noreply.github.com>
2022-10-25 23:01:07 +0530

    created size of operator.c

---
+++ C codes/size of operator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>  
+int main()  
+{  
+    int x=89;    
+    printf("size of the variable x is %d", sizeof(x));  // Displaying the size of ?x? variable.  
+    printf("\nsize of the integer data type is %d",sizeof(int)); //Displaying the size of integer data type.  
+    printf("\nsize of the character data type is %d",sizeof(char)); //Displaying the size of character data type.  
+  
+    printf("\nsize of the floating data type is %d",sizeof(float)); //Displaying the size of floating data type.  
+return 0;  
+}  
